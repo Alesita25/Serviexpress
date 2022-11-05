93fa6649d55772d2e8f349acb0b41d55e58c77c6
Alesita25 <101678491+Alesita25@users.noreply.github.com>
2022-11-05 20:45:36 -0300

    camara2

--- src/app/reserva/reserva.page.ts
+++ src/app/reserva/reserva.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DbserviceService } from 'src/app/service/dbservice.service';
-
+import { CamaraService } from 'src/app/service/camara.service';
 @Component({
   selector: 'app-reserva',
   templateUrl: './reserva.page.html',
@@ -12,12 +12,17 @@ export class ReservaPage implements OnInit {
   servicio = "";
   fecha = "";
 
-  constructor(private dbservice: DbserviceService, private router: Router) { }
+  constructor(private dbservice: DbserviceService, private router: Router, private camara:CamaraService) { }
 
   guardar() {
     this.dbservice.addReserva(this.servicio,this.fecha);
     this.dbservice.presentToast("Reserva ingresada correctamente");
     this.router.navigate(['/cliente-home']);
+  }
+
+  takePicture(){
+    this.camara.takePicture;
+    this.dbservice.presentToast("Ingreso correcto");
   }
 
   ngOnInit() {
